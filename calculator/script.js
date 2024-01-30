d60fb9a3780b0070c4f799b57268392bcce23792
siued <matej.kucera.01@gmail.com>
2024-01-30 11:26:35 +0100

    started working on calculator

---
+++ calculator/script.js
@@ -0,0 +1,18 @@
+function operate(a, operand, b) {
+    switch (operand) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+    }
+}
+
+function initialize() {
+
+}
+
+initialize();
